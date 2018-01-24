760a0319602cf132c7ba14215c569f0b1ee44e99
Mars Wong <marswong618@gmail.com>
2018-01-24 13:30:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,5 +4,16 @@ def countdown(n)
   while n > 0 do
     puts "#{n--} SECOND(S)!"
   end
+  
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  sleep n
+
+  while n > 0 do
+    puts "#{n--} SECOND(S)!"
+  end
+  
   "HAPPY NEW YEAR!"
 end
